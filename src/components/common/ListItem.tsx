e50477c95821802700a4c6af44d6ce182c85f14d
Waheed Uddin Ahmed <dmc4719@gmail.com>
2023-03-22 02:43:02 +0600

    Add list items in early access section

--- src/components/common/ListItem.tsx
+++ src/components/common/ListItem.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { howItWorksListItemVariants } from '../../../utils/Animations';
+import { howItWorksListItemVariants } from '../../utils/Animations';
 
 type ListItemProps = {
     title: string;
